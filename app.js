7698c5fa9bbc7a893885c8d4366a7bcc79920390
Santhosh1507 <webdesign1507@gmail.com>
2024-09-12 11:05:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import connectionRoutes from "./routes/connection.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/notifications", notificationRoutes);
+app.use("/api/v1/connections", connectionRoutes);
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+})
+
+app.listen(3000, () => {
+  console.log(`Server running on http://localhost:3000`);
+  connectDB();
+});
